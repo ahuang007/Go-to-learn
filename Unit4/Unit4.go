59e8c6426ec0c859a10c4e50c5b13ba14c34ab26
ahuang007 <419637330@qq.com>
2018-11-25 23:19:27 +0800

    const usage

--- Unit4/Unit4.go
+++ Unit4/Unit4.go
@@ -15,16 +15,25 @@ const (
 //常量表达式中，函数必须是内置函数，否则编译不过
 const (
 	a1 = "abc"
-	a2 = len(a1)
-	a3 = unsafe.Sizeof(a2)
+	a2 = len(a1)           // 3 字符串长度
+	a3 = unsafe.Sizeof(a1) // 16
+	//字符串类型在go里是个结构，包含指向底层数组的指针和长度
+	//这两部分每部分都是8字节，所以字符串长度为16字节
 )
 
 // iota 特殊常量 在const关键字出现时将被重置为0（const内部的第一行之前）
 // const 每新增一行常量声明将使iota计数一次(iota可以理解为const语句块中的行索引)
 const (
-	b1 = iota
-	b2
-	b3
+	b1 = iota // 0
+	b2        // 1
+	b3        // 2
+)
+
+const (
+	d1 = 1 << iota //1
+	d2 = 3 << iota //6
+	d3             //12
+	d4             //24
 )
 
 func main() {
@@ -37,10 +46,25 @@ func main() {
 	fmt.Printf("面积为：%d", area)
 	println()
 	println(a, b, c)
-	println(a1, a2, a3)
+	println(a1, a2, a3, a4)
 
 	arr := []int{1, 2, 3} //var arr = [3]int{1, 2, 3}
 	println(cap(arr))     // cap()计算数组长度
 
 	println(b1, b2, b3)
+
+	const (
+		c1 = iota // 0
+		c2        // 1
+		c3        // 2
+		c4 = "ha" // "ha" iota += 1
+		c5        // "ha" iota += 1
+		c6 = 100  // 100  iota += 1
+		c7        // 100  iota += 1
+		c8 = iota // 7, 恢复计数
+		c9        // 8
+	)
+
+	fmt.Println(c1, c2, c3, c4, c5, c6, c7, c8, c9)
+	fmt.Println(d1, d2, d3, d4)
 }
