988dc7a6d2c7a55683afd2423eca0579c4af2efc
ahuang007 <419637330@qq.com>
2018-11-27 23:23:03 +0800

    move comments to the top of file

--- Unit6/Unit6.go
+++ Unit6/Unit6.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+/*
+switch 语句用于基于不同条件执行不同动作，每一个 case 分支都是唯一的，从上直下逐一测试，直到匹配为止。。
+
+switch 语句执行的过程从上至下，直到找到匹配项，匹配项后面也不需要再加break
+switch var1 {
+    case val1:
+        ...
+    case val2:
+        ...
+    default:
+        ...
+}
+变量 var1 可以是任何类型，而 val1 和 val2 则可以是同类型的任意值。
+类型不被局限于常量或整数，但必须是相同的类型；或者最终结果为相同类型的表达式。
+可以同时测试多个可能符合条件的值，使用逗号分割它们，例如：case val1, val2, val3。
+
+Type Switch
+switch 语句还可以被用于 type-switch 来判断某个 interface 变量中实际存储的变量类型。
+*/
+
 func main() {
 	var grade string = "B"
 	var marks int = 90
@@ -58,23 +78,3 @@ func main() {
 		fmt.Printf("未知类型\n")
 	}
 }
-
-/*
-switch 语句用于基于不同条件执行不同动作，每一个 case 分支都是唯一的，从上直下逐一测试，直到匹配为止。。
-
-switch 语句执行的过程从上至下，直到找到匹配项，匹配项后面也不需要再加break
-switch var1 {
-    case val1:
-        ...
-    case val2:
-        ...
-    default:
-        ...
-}
-变量 var1 可以是任何类型，而 val1 和 val2 则可以是同类型的任意值。
-类型不被局限于常量或整数，但必须是相同的类型；或者最终结果为相同类型的表达式。
-可以同时测试多个可能符合条件的值，使用逗号分割它们，例如：case val1, val2, val3。
-
-Type Switch
-switch 语句还可以被用于 type-switch 来判断某个 interface 变量中实际存储的变量类型。
-*/
